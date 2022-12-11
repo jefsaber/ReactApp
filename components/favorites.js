a9711166a20e2cd23f9d88a4b83971cd3ca26b10
Jef Sbaer <jef.saber@hotmail.com>
2022-12-12 00:46:37 +0200

    .

--- components/favorites.js
+++ components/favorites.js
@@ -1,69 +1,135 @@
-import { StyleSheet, Text, View} from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
 import Productlist from "./productlist";
 import { Button } from "react-native-paper";
 import { getUserData } from "./homepage";
-
+import { db } from "../firebase/firebase";
+import { doc, updateDoc } from "firebase/firestore";
 //import { data } from "../assets/Data";
 import { getAllProducts } from "../App.js";
-
+import MaterialIcons from "@expo/vector-icons/Ionicons";
 
 const Favorites = ({ navigation }) => {
   let AllProducts = getAllProducts();
   const tmpUser = getUserData();
-
   const data = AllProducts.filter((element) => {
     return tmpUser.Favorites.includes(element.id);
   });
-  const iconfunction = () => {
-
-
+  //console.log(data);
+  const [Favorites, setFavorites] = useState(data);
+  //console.log(Favorites);
+  const RemoveFavorites = async (item) => {
+    console.log(item.id);
+    const docRef = doc(db, "Users", tmpUser.id);
+    tmpUser.Favorites.splice(tmpUser.Favorites.indexOf(item.id));
+    console.log(tmpUser.Favorites);
+    const Favorites = {
+      Favorites: tmpUser.Favorites,
+    };
+    updateDoc(docRef, Favorites)
+      .then(() => {
+        setFavorites(tmpUser.Favorites);
+        console.log("added successfuly");
+        alert("Favorites Successfuly changed");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
     <View style={styles.container}>
-      <View style={styles.RecentCont}>
-        {tmpUser.Favorites.length != 0 ? (
-          <Productlist
-            icon="heart"
-            title="My Favorites"
-            navigation={navigation}
-            data={data}
-            iconfunction={iconfunction}
-          />
-        ) : (
-          <View
+      <View>
+        <Text style={styles.RecentText}>Favorites</Text>
+      </View>
+      {tmpUser.Favorites.length != 0 ? (
+        <ScrollView>
+          <View>
+            <View style={styles.ProductsCont}>
+              {Favorites.map((item) => {
+                console.log(item);
+                return (
+                  <TouchableOpacity
+                    delayPressIn={50}
+                    key={item.id}
+                    activeOpacity={0.4}
+                  >
+                    <View style={styles.ProductCont}>
+                      <View style={styles.ProductDetails}>
+                        <Image
+                          resizeMode="contain"
+                          source={require("../assets/nike1.png")}
+                          style={styles.ProductImage}
+                        />
+                        <View>
+                          <Text style={styles.ProductTitle}>{item.Title}</Text>
+                          <Text style={styles.ProductCategory}>
+                            {item.Category}
+                          </Text>
+                        </View>
+                      </View>
+                      <View style={{ flexDirection: "row" }}>
+                        <View>
+                          <Text>1pc</Text>
+                        </View>
+                        <View style={styles.PriceCont}>
+                          <Text>${item.Price}</Text>
+                        </View>
+                      </View>
+                      <TouchableOpacity
+                        delayPressIn={50}
+                        style={styles.CloseButton}
+                        hitSlop={{ top: 9, bottom: 9, left: 9, right: 9 }}
+                        onPress={() => RemoveFavorites(item)}
+                      >
+                        <MaterialIcons name="heart" size={24} />
+                      </TouchableOpacity>
+                    </View>
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+          </View>
+        </ScrollView>
+      ) : (
+        <View
+          style={{
+            height: "100%",
+          }}
+        >
+          <Text
             style={{
-              height: "100%",
+              marginTop: "50%",
+              textAlign: "center",
+              fontWeight: "bold",
+              fontSize: 22,
+              marginBottom: 20,
             }}
           >
-            <Text
-              style={{
-                marginTop: "50%",
-                textAlign: "center",
-                fontWeight: "bold",
-                fontSize: 22,
-                marginBottom: 20,
-              }}
-            >
-              Your Favorites List Is Empty
-            </Text>
-            <Button
-              style={{
-                backgroundColor: "#6E9FFF",
-                marginRight: 20,
-                marginLeft: 20,
-                padding: 6,
-              }}
-              labelStyle={{ fontWeight: "bold" }}
-              mode="contained"
-              title="Go to Shop"
-              onPress={() => navigation.navigate("Shop")}
-            >
-              go to shop
-            </Button>
-          </View>
-        )}
-      </View>
+            Your Favorites List Is Empty
+          </Text>
+          <Button
+            style={{
+              backgroundColor: "#6E9FFF",
+              marginRight: 20,
+              marginLeft: 20,
+              padding: 6,
+            }}
+            labelStyle={{ fontWeight: "bold" }}
+            mode="contained"
+            title="Go to Shop"
+            onPress={() => navigation.navigate("Shop")}
+          >
+            go to shop
+          </Button>
+        </View>
+      )}
     </View>
   );
 };
@@ -75,10 +141,47 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     backgroundColor: "#ffffff",
+    paddingHorizontal: 20,
   },
   RecentCont: {
-    marginHorizontal: 20,
+    paddingLeft: 20,
     marginBottom: 15,
     paddingTop: 15,
+  },
+  RecentText: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  ProductDetails: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "55%",
+    height: "100%",
+  },
+  ProductsCont: {
+    marginTop: 10,
+  },
+  ProductCont: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
+    width: "96%",
+  },
+  ProductImage: {
+    width: 90,
+    height: 60,
+  },
+  ProductTitle: {
+    fontWeight: "bold",
+    fontSize: 14,
+    marginBottom: 5,
+  },
+  ProductCategory: {
+    fontSize: 12,
+  },
+  PriceCont: {
+    marginLeft: 10,
   },
 });
